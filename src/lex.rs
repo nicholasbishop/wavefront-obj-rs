22329af57c2895c1aa95c0764c29587dc008b003
Nicholas Bishop <nicholasbishop@gmail.com>
2014-11-29 00:00:53 -0500

    Remove option around token next result

--- src/lex.rs
+++ src/lex.rs
@@ -1,6 +1,7 @@
 extern crate std;
 
 use std::io::BufferedReader;
+use std::io::IoErrorKind;
 use std::io::IoResult;
 
 #[deriving(PartialEq, Show)]
@@ -40,7 +41,8 @@ pub struct TokenIterator<R> {
     buffered_reader: BufferedReader<R>,
     state: State,
     buffer: String,
-    token_type: Option<TokenType>
+    token_type: Option<TokenType>,
+    err: Option<std::io::IoError>
 }
 
 fn comment_char(c: char) -> bool {
@@ -107,43 +109,46 @@ impl<R: Reader> TokenIterator<R> {
         }
     }
 
-    fn next(&mut self) -> Option<IoResult<Token>> {
-        let mut result = None;
-
+    fn next(&mut self) -> IoResult<Token> {
         if self.token_type.is_some() {
             self.token_type = None;
             self.buffer.clear();
         }
 
-        while result.is_none() {
+        if self.state == State::EndOfFile {
+            // TODO(bishop)
+            return Result::Err(self.err.as_ref().unwrap().clone());
+        }
+
+        loop {
             match self.buffered_reader.read_char() {
                 Ok(c) => {
                     self.push_char(c);
 
                     if let Some(token_type) = self.token_type {
-                        return Some(Result::Ok(Token::new(token_type,
-                                                          self.buffer.as_slice())));
+                        return Result::Ok(Token::new(token_type,
+                                                     self.buffer.as_slice()));
                     }
                 }
                 Err(ref e) => {
+                    self.err = Some(e.clone());
                     if e.kind == std::io::IoErrorKind::EndOfFile {
                         self.push_char('\n');
 
                         // TODO(bishop): deduplicate
                         if let Some(token_type) = self.token_type {
-                            return Some(Result::Ok(Token::new(token_type,
-                                                              self.buffer.as_slice())));
+                            self.state = State::EndOfFile;
+                            return Result::Ok(Token::new(token_type,
+                                                         self.buffer.as_slice()));
                         } else {
-                            return None;
+                            return Result::Err(e.clone());
                         }
 
                     }
-                    result = Some(Result::Err(e.clone()));
+                    return Result::Err(e.clone());
                 }
             }
         }
-
-        result
     }
 }
 
@@ -152,7 +157,8 @@ pub fn read_obj<R: Reader>(reader: R) -> TokenIterator<R> {
         buffered_reader: BufferedReader::new(reader),
         state: State::StartOfLine,
         buffer: String::new(),
-        token_type: None
+        token_type: None,
+        err: None
     };
     iter
 }
@@ -161,41 +167,50 @@ fn str_reader(s: &'static str) -> std::io::BufReader {
     std::io::BufReader::new(s.as_bytes())
 }
 
+// TODO
+fn iter_eof<R: Reader>(iter: &mut TokenIterator<R>) -> bool {
+    if let Result::Err(e) = iter.next() {
+        e.kind == IoErrorKind::EndOfFile
+    } else {
+        false
+    }
+}
+
 #[test]
 fn test_tag() {
     let mut iter = read_obj(str_reader("a\n"));
-    assert!(iter.next().unwrap().unwrap() == Token::Tag("a"));
-    assert!(iter.next() == None);
+    assert!(iter.next().unwrap() == Token::Tag("a"));
+    assert!(iter_eof(&mut iter));
 }
 
 #[test]
 fn test_tag_and_arguments() {
     let mut iter = read_obj(str_reader("a b c\n"));
-    assert!(iter.next().unwrap().unwrap() == Token::Tag("a"));
-    assert!(iter.next().unwrap().unwrap() == Token::Argument("b"));
-    assert!(iter.next().unwrap().unwrap() == Token::Argument("c"));
-    assert!(iter.next() == None);
+    assert!(iter.next().unwrap() == Token::Tag("a"));
+    assert!(iter.next().unwrap() == Token::Argument("b"));
+    assert!(iter.next().unwrap() == Token::Argument("c"));
+    assert!(iter_eof(&mut iter));
 }
 
 #[test]
 fn test_tag_no_newline() {
     let mut iter = read_obj(str_reader("a"));
-    assert!(iter.next().unwrap().unwrap() == Token::Tag("a"));
-    assert!(iter.next() == None);
+    assert!(iter.next().unwrap() == Token::Tag("a"));
+    assert!(iter_eof(&mut iter));
 }
 
 #[test]
 fn test_line_comment() {
     let mut iter = read_obj(str_reader("# comment\n"));
-    assert!(iter.next().unwrap().unwrap() == Token::Comment(" comment"));
-    assert!(iter.next() == None);
+    assert!(iter.next().unwrap() == Token::Comment(" comment"));
+    assert!(iter_eof(&mut iter));
 }
 
 
 #[test]
 fn test_comment_after_tag() {
     let mut iter = read_obj(str_reader("v # comment\n"));
-    assert!(iter.next().unwrap().unwrap() == Token::Tag("v"));
-    assert!(iter.next().unwrap().unwrap() == Token::Comment(" comment"));
-    assert!(iter.next() == None);
+    assert!(iter.next().unwrap() == Token::Tag("v"));
+    assert!(iter.next().unwrap() == Token::Comment(" comment"));
+    assert!(iter_eof(&mut iter));
 }
